16bc2eec342a40146cef2e8e724f2cc269dd9e1a
Nathan <nathandpanzer@gmail.com>
2019-06-18 16:20:06 -0600

    Added Evoker Fangs Rotatable entity

--- src/main/java/com/thekingelessar/minecraftmagic/common/entity/EntityEvokerFangsRotatable.java
+++ src/main/java/com/thekingelessar/minecraftmagic/common/entity/EntityEvokerFangsRotatable.java
@@ -1,5 +1,48 @@
 package com.thekingelessar.minecraftmagic.common.entity;
 
-public class EntityEvokerFangsRotatable
+import io.netty.handler.codec.DecoderException;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.projectile.EntityEvokerFangs;
+import net.minecraft.network.PacketBuffer;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.common.registry.IEntityAdditionalSpawnData;
+
+public class EntityEvokerFangsRotatable extends EntityEvokerFangs implements IEntityAdditionalSpawnData
 {
+    
+    public String blockSide;
+    
+    
+    public EntityEvokerFangsRotatable(World worldIn)
+    {
+        super(worldIn);
+    }
+    
+    @Override
+    public EntityType<?> getType()
+    {
+        return ModEntities.evokerfangsrotatable;
+    }
+    
+    @Override
+    public void writeSpawnData(PacketBuffer buffer)
+    {
+        if (this.blockSide != null)
+        {
+            buffer.writeString(this.blockSide);
+        }
+    }
+    
+    @Override
+    public void readSpawnData(PacketBuffer additionalData)
+    {
+        try
+        {
+            this.blockSide = additionalData.readString(8);
+        }
+        catch (DecoderException exeption)
+        {
+            this.blockSide = null;
+        }
+    }
 }
--- src/main/java/com/thekingelessar/minecraftmagic/common/register/RegisterEntity.java
+++ src/main/java/com/thekingelessar/minecraftmagic/common/register/RegisterEntity.java
@@ -1,5 +1,21 @@
 package com.thekingelessar.minecraftmagic.common.register;
 
+import com.thekingelessar.minecraftmagic.MinecraftMagic;
+import com.thekingelessar.minecraftmagic.common.entity.EntityEvokerFangsRotatable;
+import net.minecraft.entity.EntityType;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.IForgeRegistry;
+
+@Mod.EventBusSubscriber (bus = Mod.EventBusSubscriber.Bus.MOD)
 public class RegisterEntity
 {
+    @SubscribeEvent
+    public static void onEntitiesRegistry(final RegistryEvent.Register<EntityType<?>> entityRegistryEvent)
+    {
+        IForgeRegistry<EntityType<?>> registry = entityRegistryEvent.getRegistry();
+        
+        registry.register(EntityType.Builder.create(EntityEvokerFangsRotatable.class, EntityEvokerFangsRotatable::new).tracker(60, 24, true).disableSerialization().build("evokerfangsrotatable").setRegistryName(MinecraftMagic.modid, "evokerfangsrotatable"));
+    }
 }
