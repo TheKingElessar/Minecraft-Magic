96b48236abe44d95dca57e1d8c2ac2b3c98f2cfa
Nathan <nathandpanzer@gmail.com>
2019-06-18 16:26:01 -0600

    Added items for spells Conjure Fang Row and Conjure Fang Circle

--- src/main/java/com/thekingelessar/minecraftmagic/common/item/ModItems.java
+++ src/main/java/com/thekingelessar/minecraftmagic/common/item/ModItems.java
@@ -1,11 +1,14 @@
-package com.thekingelessar.minecraftmagic.item;
+package com.thekingelessar.minecraftmagic.common.item;
 
 import com.thekingelessar.minecraftmagic.MinecraftMagic;
 import net.minecraft.item.Item;
 import net.minecraftforge.registries.ObjectHolder;
 
-@ObjectHolder(MinecraftMagic.modid)
-public class ModItems {
+@ObjectHolder (MinecraftMagic.modid)
+public class ModItems
+{
     public static final Item MODITEM = null;
     public static final Item CONJUREFANG = null;
+    public static final Item CONJUREFANGROW = null;
+    public static final Item CONJUREFANGCIRCLE = null;
 }
