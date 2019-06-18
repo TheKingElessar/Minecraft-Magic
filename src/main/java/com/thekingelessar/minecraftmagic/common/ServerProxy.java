55044228052aa142ab52332e9862e7b0bbee9785
Nathan <nathandpanzer@gmail.com>
2019-06-18 16:17:03 -0600

    Reworked proxies, added Evoker Fangs Rotatable entity

--- src/main/java/com/thekingelessar/minecraftmagic/common/ServerProxy.java
+++ src/main/java/com/thekingelessar/minecraftmagic/common/ServerProxy.java
@@ -4,11 +4,12 @@ import net.minecraft.world.World;
 
 public class ServerProxy
 {
-
+    
     public void preInit()
     {
+        System.out.println("Server proxy preInit");
     }
-
+    
     public World getClientWorld()
     {
         return null;
