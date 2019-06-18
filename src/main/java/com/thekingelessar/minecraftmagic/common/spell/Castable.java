9610c5aacdf82be3ba3f335cc8f1869748349095
Nathan <nathandpanzer@gmail.com>
2019-06-18 16:24:52 -0600

    Added framework for spells

--- src/main/java/com/thekingelessar/minecraftmagic/common/spell/Castable.java
+++ src/main/java/com/thekingelessar/minecraftmagic/common/spell/Castable.java
@@ -3,12 +3,12 @@ package com.thekingelessar.minecraftmagic.common.spell;
 import com.thekingelessar.minecraftmagic.common.spell.target.ITarget;
 import net.minecraft.entity.Entity;
 
-public abstract class Castable {
-
+public abstract class Castable
+{
+    
     protected Entity caster;
     public ITarget target = null;
-
-
-    public abstract void cast();
-
+    
+    public abstract void castClient();
+    
 }
