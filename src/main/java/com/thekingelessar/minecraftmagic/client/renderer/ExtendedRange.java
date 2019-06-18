2b9fbaf9847198c01fdcbe1a68db5733c4cb5e3f
Nathan <nathandpanzer@gmail.com>
2019-06-18 16:19:05 -0600

    Formatting

--- src/main/java/com/thekingelessar/minecraftmagic/client/renderer/ExtendedRange.java
+++ src/main/java/com/thekingelessar/minecraftmagic/client/renderer/ExtendedRange.java
@@ -11,8 +11,9 @@ import net.minecraft.world.chunk.IChunkProvider;
 import javax.annotation.Nullable;
 import java.util.List;
 
-public class ExtendedRange {
-
+public class ExtendedRange
+{
+    
     private Minecraft mc;
     public RayTraceResult mcObjectMouseOver;
     private final double range;
@@ -21,20 +22,21 @@ public class ExtendedRange {
     private Float partialTicks;
     public boolean airTargeted;
     public EnumFacing blockSideHit;
-
+    
     private IChunkProvider chunkProvider;
-
+    
     public ExtendedRange(@Nullable Float partialTicks, double range, RayTraceFluidMode fluidMode, Minecraft mc, Entity entity)
     {
         this.mc = mc;
-        if(partialTicks == null) {
+        if (partialTicks == null)
+        {
             this.partialTicks = 1.0F;
         }
         this.range = range;
         this.fluidMode = fluidMode;
         this.requestingEntity = entity;
     }
-
+    
     public void getMouseOver()
     {
         if (requestingEntity != null)
@@ -44,14 +46,14 @@ public class ExtendedRange {
                 this.mc.profiler.startSection("pick");
                 Entity pointedEntity = null;
                 double d0 = range; // block reach distance. default 5.0D, max 1024.0D;
-
+                
                 this.mcObjectMouseOver = requestingEntity.rayTrace(d0, partialTicks, fluidMode);
                 BlockPos blockPos = this.mcObjectMouseOver.getBlockPos();
                 IBlockState state = mc.world.getBlockState(blockPos);
                 this.blockSideHit = mcObjectMouseOver.sideHit;
-
+                
                 this.airTargeted = state.isAir(mc.world, blockPos);
-
+                
                 Vec3d vec3d = requestingEntity.getEyePosition(partialTicks);
                 boolean flag = false;
                 int i = 3;
@@ -60,19 +62,19 @@ public class ExtendedRange {
                 {
                     flag = true;
                 }
-
+                
                 if (this.mcObjectMouseOver != null)
                 {
                     d1 = this.mcObjectMouseOver.hitVec.distanceTo(vec3d); // distance between ray trace and eye position
                 }
-
+                
                 Vec3d vec3d1 = requestingEntity.getLook(1.0F); // get vector from angle of look
                 Vec3d vec3d2 = vec3d.add(vec3d1.x * d0, vec3d1.y * d0, vec3d1.z * d0); // add range multiplied by where entity is looking
                 Vec3d vec3d3 = null;
                 float f = 1.0F;
                 List<Entity> list = this.mc.world.getEntitiesInAABBexcluding(requestingEntity, requestingEntity.getBoundingBox().expand(vec3d1.x * d0, vec3d1.y * d0, vec3d1.z * d0).grow(1.0D, 1.0D, 1.0D), EntitySelectors.NOT_SPECTATING.and(Entity::canBeCollidedWith));
                 double d2 = d1; // d1 is either range or distance between ray trace and eye position
-
+                
                 for (Entity entity1 : list)
                 {
                     AxisAlignedBB axisalignedbb = entity1.getBoundingBox().grow((double) entity1.getCollisionBorderSize());
@@ -83,11 +85,12 @@ public class ExtendedRange {
                         {
                             pointedEntity = entity1;
                             this.airTargeted = false;
-
+                            
                             vec3d3 = raytraceresult == null ? vec3d : raytraceresult.hitVec;
                             d2 = 0.0D;
                         }
-                    } else if (raytraceresult != null) // run when entity is targeted...
+                    }
+                    else if (raytraceresult != null) // run when entity is targeted...
                     {
                         double d3 = vec3d.distanceTo(raytraceresult.hitVec); // distance between eye position and range location
                         if (d3 < d2 || d2 == 0.0D)
@@ -98,32 +101,33 @@ public class ExtendedRange {
                                 {
                                     pointedEntity = entity1;
                                     this.airTargeted = false;
-
+                                    
                                     vec3d3 = raytraceresult.hitVec;
                                 }
-                            } else
+                            }
+                            else
                             {
                                 pointedEntity = entity1;
                                 this.airTargeted = false;
-
+                                
                                 vec3d3 = raytraceresult.hitVec;
                                 d2 = d3;
                             }
                         }
                     }
-
+                    
                 }
-
+                
                 if (pointedEntity != null && flag && vec3d.distanceTo(vec3d3) > 3.0D) // if * AND reach is greater than 3 blocks AND
                 {
-                    this.mcObjectMouseOver = new RayTraceResult(RayTraceResult.Type.MISS, vec3d3, (EnumFacing)null, new BlockPos(vec3d3));
+                    this.mcObjectMouseOver = new RayTraceResult(RayTraceResult.Type.MISS, vec3d3, (EnumFacing) null, new BlockPos(vec3d3));
                 }
-
+                
                 if (pointedEntity != null && (d2 < d1 || this.mcObjectMouseOver == null))
                 {
                     this.mcObjectMouseOver = new RayTraceResult(pointedEntity, vec3d3);
                 }
-
+                
                 this.mc.profiler.endSection();
             }
         }
--- src/main/java/com/thekingelessar/minecraftmagic/common/network/packets/PacketGlowSingleEntity.java
+++ src/main/java/com/thekingelessar/minecraftmagic/common/network/packets/PacketGlowSingleEntity.java
@@ -10,49 +10,53 @@ import net.minecraftforge.fml.network.NetworkEvent;
 
 import java.util.function.Supplier;
 
-public class PacketGlowSingleEntity {
-
+public class PacketGlowSingleEntity
+{
+    
     private final String UUID;
-
+    
     public PacketGlowSingleEntity(String UUID)
     {
         this.UUID = UUID;
     }
-
+    
     public static void encode(PacketGlowSingleEntity msg, PacketBuffer buf)
     {
         buf.writeString(msg.UUID);
     }
-
+    
     public static PacketGlowSingleEntity decode(PacketBuffer buf)
     {
         String UUID = buf.readString(36);
-
+        
         return new PacketGlowSingleEntity(UUID);
     }
-
+    
     public static class Handler
     {
         public static void handle(final PacketGlowSingleEntity message, Supplier<NetworkEvent.Context> ctx)
         {
-            ctx.get().enqueueWork(() -> {
-
+            ctx.get().enqueueWork(() ->
+            {
+                
                 java.util.UUID uuid = java.util.UUID.fromString(message.UUID);
                 NetworkEvent.Context context = ctx.get();
                 World world = context.getSender().world;
-
-                for (Entity entityInWorld : world.loadedEntityList) {
-
-                    if(entityInWorld.getUniqueID().equals(uuid)) {
+                
+                for (Entity entityInWorld : world.loadedEntityList)
+                {
+                    
+                    if (entityInWorld.getUniqueID().equals(uuid))
+                    {
                         EntityLiving livingEntity = (EntityLiving) entityInWorld;
                         PotionEffect glowing = new PotionEffect(MobEffects.GLOWING, 100);
                         livingEntity.addPotionEffect(glowing);
                     }
                 }
             });
-
+            
             ctx.get().setPacketHandled(true);
         }
     }
-
+    
 }
