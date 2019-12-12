a9307eeb1774b14a475089722e203dd88fb2bcee
TheKingElessar <wasabimath@gmail.com>
2019-12-12 10:31:53 -0700

    Added flowchart

--- src/main/java/com/thekingelessar/minecraftmagic/common/item/spell/evocation/ItemFirebolt.java
+++ src/main/java/com/thekingelessar/minecraftmagic/common/item/spell/evocation/ItemFirebolt.java
@@ -5,6 +5,7 @@ import com.thekingelessar.minecraftmagic.common.spell.spells.evocation.SpellFire
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemUseContext;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumHand;
@@ -38,6 +39,11 @@ public class ItemFirebolt extends Item
             playerIn.setActiveHand(handIn); // Shield function
             return new ActionResult<>(EnumActionResult.SUCCESS, itemstack); // Shield function
         }
+    }
+    
+    @Override
+    public EnumActionResult onItemUse(ItemUseContext context) {
+        return EnumActionResult.PASS;
     }
     
 }
